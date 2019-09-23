cdc4ce3ebb0b99f3fb2c39992b82a1f4c7fe9133
Clayton Ritchie <cr071417@ohio.edu>
2019-09-23 16:22:00 -0400

    Moved html into templates

--- NewSite/recipes/urls.py
+++ NewSite/recipes/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('signin/', views.signIn, name='accountSignIn'),
+    path('signup/', views.signUp, name='accountSignUp'),
     path('', views.index, name='index'),
 ]
