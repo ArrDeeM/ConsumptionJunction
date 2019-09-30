319ac6116c80400b78ce33b8b001236cb1661851
zach922 <zacharyericschmitz@gmail.com>
2019-09-30 12:11:20 -0400

    adding account model

--- NewSite/NewSite/urls.py
+++ NewSite/NewSite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('recipes.urls')),
+    path('recipes/', include('recipes.urls')),
     path('admin/', admin.site.urls),
 ]
