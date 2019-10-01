1aad33ca0172a778b5864b0d6f564c2d86c0db1d
Clayton Ritchie <cr071417@ohio.edu>
2019-10-01 15:58:40 -0400

    adding recipe model and migrating

--- NewSite/recipes/models.py
+++ NewSite/recipes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Recipes(models.Model):
+
+    #fields for data
+    recipe_name = models.CharField(max_length=20, help_text='Name of provided recipe')
+    #recipe_image = models.ImageField()
+    #Queryset was acting strange with images for some reason
+    ingredients = models.TextField()
+    steps = models.TextField()
+
+    def __str__(self):
+        return self.recipe_name
